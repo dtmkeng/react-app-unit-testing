0b970e7b291a451a4b6ac4d5ab2e5440392939af
dtmkeng <phumaisut@gmail.com>
2022-07-16 23:52:28 +0700

    move file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import Counter from "components/Counter";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home <Counter inital={0}></Counter>
+    </div>
+  );
 };
 const About = () => {
   return <div>About</div>;
@@ -10,6 +15,7 @@ const About = () => {
 function App() {
   return (
     <div className="App">
+      <h1>Hello React App</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
