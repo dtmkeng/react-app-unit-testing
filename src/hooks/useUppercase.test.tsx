e0f8a6f6d18db47e105d94c9d288ea5a08192171
dtmkeng <phumaisut@gmail.com>
2022-07-17 15:00:21 +0700

    test hook

---
+++ src/hooks/useUppercase.test.tsx
@@ -0,0 +1,26 @@
+import { act, renderHook } from "@testing-library/react-hooks";
+import { useUppercase } from "./useUpperCase";
+
+describe("UseUppercase", () => {
+  it("returns current initial value, uppercased", () => {
+    const { result } = renderHook(() => useUppercase("Test string"));
+
+    expect(result.current.value).toEqual("TEST STRING");
+  });
+
+  it("changes the `value` and uppercases it", () => {
+    const { result } = renderHook(() => useUppercase("Test string"));
+
+    act(() => result.current.update("Updated"));
+    expect(result.current.value).toEqual("UPDATED");
+  });
+
+  it("updates the value", () => {
+    const { result, rerender } = renderHook(({ text }) => useUppercase(text), {
+      initialProps: { text: "Test String" },
+    });
+
+    rerender({ text: "Updated!" });
+    expect(result.current.value).toEqual("UPDATED!");
+  });
+});
